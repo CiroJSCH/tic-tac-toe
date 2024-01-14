b898ca813e0988dd2ef606251bf96163fab03e3a
CiroJSCH <cirojsh23@gmail.com>
2024-01-14 19:11:04 -0300

    Agregar archivos de proyecto.

---
+++ Game.cs
@@ -0,0 +1,10 @@
+﻿namespace tic_tac_toe
+{
+    internal class Game
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tic_tac_toe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.Hello();
+        }
+    }
+}
